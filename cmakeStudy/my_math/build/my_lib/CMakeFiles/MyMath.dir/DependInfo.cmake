05c08b6a057f107196318fb56638e1058e345a61
赵泽浩 <3516422381@qq.com>
2025-12-29 13:22:20 +0800

    cmake静态库相关

---
+++ cmakeStudy/my_math/build/my_lib/CMakeFiles/MyMath.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/zzh/linux/cmakeStudy/my_math/my_lib/src/add.cpp" "my_lib/CMakeFiles/MyMath.dir/src/add.cpp.o" "gcc" "my_lib/CMakeFiles/MyMath.dir/src/add.cpp.o.d"
+  "/home/zzh/linux/cmakeStudy/my_math/my_lib/src/sub.cpp" "my_lib/CMakeFiles/MyMath.dir/src/sub.cpp.o" "gcc" "my_lib/CMakeFiles/MyMath.dir/src/sub.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmakeStudy/my_math/build/my_lib/CMakeFiles/MyMath.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../lib/libMyMath.a"
+  "../lib/libMyMath.pdb"
+  "CMakeFiles/MyMath.dir/src/add.cpp.o"
+  "CMakeFiles/MyMath.dir/src/add.cpp.o.d"
+  "CMakeFiles/MyMath.dir/src/sub.cpp.o"
+  "CMakeFiles/MyMath.dir/src/sub.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyMath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
