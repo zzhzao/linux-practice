66b6dcd71e093d692ff65a053e378b8dad4a262f
赵泽浩 <3516422381@qq.com>
2026-04-20 15:21:38 +0800

    select多路转接的优化

--- study67/selectServer/selectServer.hpp
+++ study67/selectServer/selectServer.hpp
@@ -24,20 +24,14 @@ public:
   }
   ~selectServer() {}
 
-  void HandlerEvent(fd_set &rfds) {
-    for (int i = 0; i < gfdnum; i++) {
-      if (_rfdset[i] == gdefaultfd) {
-        continue;
-      } else {
-        if (FD_ISSET(_rfdset[i], &rfds)) {
-          if (_rfdset[i] == _listensock->Sockfd()) {
-            // 监听套接字就绪
+  void Accepter()
+  {
+// 监听套接字就绪
             LOG(LogLevel::WARNING) << "listensocketfd event ready";
             InetAddr clientaddr;
             int sockfd = _listensock->Accepter(clientaddr); // 在这里就不会卡住了
             LOG(LogLevel::DEBUG) << "get a new link, sockfd: " << sockfd;
             // 这时不能直接读取sockfd，因为sockfd是一个文件描述符，不能直接读取，需要使用select来等待读取事件
-            FD_SET(sockfd, &rfds);
             // 保存sockfd到数组中
             int pos = 0;
             for (; pos < gfdnum; pos++) {
@@ -49,34 +43,72 @@ public:
               LOG(LogLevel::WARNING) << "server is full";
               close(sockfd);
             }
+            else {
+                _rfdset[pos] = sockfd;
+            }
+  }
+
+void Recver(int i)
+    {
+        // 普通fd就绪了！
+        char buffer[1024];
+        ssize_t n = recv(_rfdset[i], buffer, sizeof(buffer), 0); // 这里读取会阻塞吗？？不会！！
+        if (n > 0)
+        {
+            buffer[n] = 0;
+            LOG(LogLevel::DEBUG) << "buffer : " << buffer;
+            std::string echo_string = "echo #";
+            echo_string += buffer;
+            // 我们可以直接发送数据吗？
+            send(_rfdset[i], echo_string.c_str(), echo_string.size(), 0);
+        }
+        else if (n == 0)
+        {
+            LOG(LogLevel::INFO) << "client quit: " << _rfdset[i];
+            // 0. 关闭fd
+            close(_rfdset[i]);
+            // 1. 从select移除掉fd
+            _rfdset[i] = gdefaultfd;
+        }
+        else
+        {
+            LOG(LogLevel::WARNING) << "recv error: " << _rfdset[i];
+            // 0. 关闭fd
+            close(_rfdset[i]);
+            // 1. 从select移除掉fd
+            _rfdset[i] = gdefaultfd;
+        }
+    }
+
+  void Dispatcher(fd_set &rfds) {
+    for (int i = 0; i < gfdnum; i++) {
+      if (_rfdset[i] == gdefaultfd) {
+        continue;
+      } else {
+        if (FD_ISSET(_rfdset[i], &rfds)) {
+          if (_rfdset[i] == _listensock->Sockfd()) {
+            Accepter();
           }
           else
           {
-            // 普通套接字就绪
-            char buffer[1024];
-            ssize_t n = recv(_rfdset[i], buffer, sizeof(buffer)-1, 0); // 不会阻塞
-            if(n > 0)
-            {
-                buffer[n] = 0;
-                LOG(LogLevel::DEBUG) << "buffer: " << buffer;
-                std::string echo_string = "echo #";
-                echo_string += buffer;
-                send(_rfdset[i], echo_string.c_str(), echo_string.size(), 0);
-            }
-            else if(n == 0){
-            
-            }
-            else {
-            
-            }
+            Recver(i);
           }
         }
+       }
       }
-
- 
-
-    }
-  }
+      }
+  
+  void PrintFd()
+    {
+        std::cout << "select fd list: ";
+        for(int i= 0; i < gfdnum; i++)
+        {
+            if(_rfdset[i] == gdefaultfd)
+                continue;
+            std::cout << _rfdset[i] << " ";
+        }
+        std::cout << std::endl;
+    } 
   void Loop() {
     while (!_quit) {
       fd_set rfds;
@@ -107,7 +139,7 @@ public:
         break;
       default:
         LOG(LogLevel::INFO) << "event ready";
-        HandlerEvent(rfds); // 不仅仅处理新链接，还会处理普通链接
+        Dispatcher(rfds); // 不仅仅处理新链接，还会处理普通链接
         break;
       }
     }
@@ -118,4 +150,5 @@ private:
   std::unique_ptr<Socket> _listensock;
   bool _quit;
   int _rfdset[gfdnum]; // 保存所有可读的fd集合
+
 };
